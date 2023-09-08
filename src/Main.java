93b855fddeb2a24237ec5aa19b7d5d6a0f5fd9e8
pawixjumper <pawel.czerepak@op.pl>
2023-09-08 09:30:14 +0200

    silnia

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        for (int i = 1;i <=5; i++){
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadz liczbe silni okok");
+
+        int x = scanner.nextInt();
+
+        int s = 1;
+
+        for(int i = 1;i<=x;i++){
+             s = s * i;
+
         }
+        System.out.println("silnia wynosi = " + s);
+
+
+
+
     }
 }
